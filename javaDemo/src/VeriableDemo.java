08410ab7f523dd9cdb01f95efdae5c063100b31c
iMac <magicya20@163.com>
2022-06-28 11:38:44 +0800

    upload1
    upload1

---
+++ javaDemo/src/VeriableDemo.java
@@ -0,0 +1,16 @@
+public class VeriableDemo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        //整数，浮点数，字符，字符串
+        int intA = 1;
+        int intB = 2;
+        int intC = 3;
+       // int intB,intC= 2; //错误，不能这样定义
+        System.out.println(intA);
+        System.out.println(intB);
+        System.out.println(intC);
+
+
+    }
+}
